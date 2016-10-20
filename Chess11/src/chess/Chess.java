60a34837e21aa45575b41aab8de6684d565bd84d
nick_scherer <nscherer30@gmail.com>
2016-10-20 09:54:16 -0400

    Added player and its interface

--- Chess11/src/chess/Chess.java
+++ Chess11/src/chess/Chess.java
@@ -5,6 +5,8 @@ public class Chess {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//initial
+		
+		
 	}
 
 }
---
+++ Chess11/src/chess/Player.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package chess;
+
+/**
+ * @author Nick
+ *
+ */
+public class Player implements PlayerAbilities {
+
+	@Override
+	public boolean isValidMove(String move, String[][] board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String[][] movePiece(String move, Piece piece, String[][] board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
