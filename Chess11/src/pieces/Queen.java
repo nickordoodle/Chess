9ed5cf7524628e8d7c92294b0ad2373dfe72c148
KartikPatel6 <kp605@scarletmail.rutgers.edu>
2016-11-11 21:32:37 -0500

    Completed Queen Movement

--- Chess11/src/pieces/Queen.java
+++ Chess11/src/pieces/Queen.java
@@ -12,8 +12,129 @@ public class Queen extends Piece {
 	}
 	
 	public boolean isValidMove(int srcRow, int srcCol, int dstRow, int dstCol, String[][] board) {
+
+		srcRow = (int) (Math.abs(srcRow - 8));
+		srcCol = (int) (srcCol - 97);
+		dstRow = (int) (Math.abs(dstRow - 8));
+		dstCol = (int) (dstCol - 97);
+
+		int rowDist = Math.abs(dstRow - srcRow);
+		int colDist = Math.abs(dstCol - srcCol);
 		
-		return true;
+		if (srcRow == dstRow) {
+			
+			if(srcCol > dstCol) {
+				for(int i = srcCol-1; i > dstCol-1; i--) {
+	
+					// check if other pieces are in the way
+					if (!board[srcRow][i].trim().equals("") && !board[srcRow][i].trim().equals("##")) {
+						return false;
+					}
+				}
+			} else {
+				for(int i = srcCol+1; i < dstCol-1; i++) {
+	
+					// check if other pieces are in the way
+					if (!board[srcRow][i].trim().equals("") && !board[srcRow][i].trim().equals("##")) {
+						return false;
+					}
+				}
+			}
+			
+			CharSequence colorToCheck;
+			if(color == 'b'){
+				colorToCheck = "w";
+			} else {
+				colorToCheck = "b";
+			}
+			
+			if (!board[dstRow][dstCol].contains(colorToCheck) && !board[dstRow][dstCol].trim().equals("") && !board[dstRow][dstCol].trim().equals("##")) {
+				return false;
+			} else {
+				return true;
+			}
+
+		} else if (srcCol == dstCol) {
+			
+			if(color == 'w') {
+				for(int i = srcRow-1; i > dstRow-1; i--) {
+	
+					// check if other pieces are in the way
+					if (!board[i][srcCol].trim().equals("") && !board[i][srcCol].trim().equals("##")) {
+						return false;
+					}
+				}
+			} else {
+				for(int i = srcRow+1; i < dstRow+1; i++) {
+	
+					// check if other pieces are in the way
+					if (!board[i][srcCol].trim().equals("") && !board[i][srcCol].trim().equals("##")) {
+						return false;
+					}
+				}
+			}
+			
+			CharSequence colorToCheck;
+			if(color == 'b'){
+				colorToCheck = "w";
+			} else {
+				colorToCheck = "b";
+			}
+			
+			if (!board[dstRow][dstCol].contains(colorToCheck) && !board[dstRow][dstCol].trim().equals("") && !board[dstRow][dstCol].trim().equals("##")) {
+				return false;
+			} else {
+				return true;
+			}
+			
+
+		} else if(rowDist == colDist) {
+			
+			CharSequence colorToCheck;
+			if(color == 'b'){
+				if (srcCol > dstCol) {
+					for(int i = 1; i < rowDist; i++) {
+						if (!board[srcRow+i][srcCol-i].trim().equals("") && !board[srcRow+i][srcCol-i].trim().equals("##")) {
+							return false;
+						}
+					}
+				} else {
+					for(int i = 1; i < rowDist; i++) {
+						if (!board[srcRow+i][srcCol+i].trim().equals("") && !board[srcRow+i][srcCol+i].trim().equals("##")) {
+							return false;
+						}
+					}
+				}
+				colorToCheck = "w";
+			} else {
+				if (srcCol > dstCol) {
+					for(int i = 1; i < rowDist; i++) {
+						System.out.println(board[srcRow-i][srcCol-i]);
+						if (!board[srcRow-i][srcCol-i].trim().equals("") && !board[srcRow-i][srcCol-i].trim().equals("##")) {
+							return false;
+						}
+					}
+				} else {
+					for(int i = 1; i < rowDist; i++) {
+						System.out.println(board[srcRow-i][srcCol+i]);
+						if (!board[srcRow-i][srcCol+i].trim().equals("") && !board[srcRow-i][srcCol+i].trim().equals("##")) {
+							return false;
+						}
+					}
+				}
+				colorToCheck = "b";
+			}
+			
+			if (!board[dstRow][dstCol].contains(colorToCheck) && !board[dstRow][dstCol].trim().equals("") && !board[dstRow][dstCol].trim().equals("##")) {
+				return false;
+			} else {
+				return true;
+			}
+			
+		} else {
+			return false;
+		}
+		
 	}
 
 }
