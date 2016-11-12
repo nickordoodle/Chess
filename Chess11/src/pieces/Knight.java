7c33b6cc7fafad384be0153ca369013035ff7058
KartikPatel6 <kp605@scarletmail.rutgers.edu>
2016-11-11 21:38:20 -0500

    Knight Movement Completed

--- Chess11/src/pieces/Knight.java
+++ Chess11/src/pieces/Knight.java
@@ -19,8 +19,34 @@ public class Knight extends Piece {
 	}
 	
 	public boolean isValidMove(int srcRow, int srcCol, int dstRow, int dstCol, String[][] board) {
+
+		srcRow = (int) (Math.abs(srcRow - 8));
+		srcCol = (int) (srcCol - 97);
+		dstRow = (int) (Math.abs(dstRow - 8));
+		dstCol = (int) (dstCol - 97);
+
+		int rowDist = Math.abs(dstRow - srcRow);
+		int colDist = Math.abs(dstCol - srcCol);
 		
-		return true;
+		if((rowDist == 2 && colDist == 1) || (rowDist == 1 && colDist == 2)) {
+			
+			CharSequence colorToCheck;
+			if(color == 'b'){
+				colorToCheck = "w";
+			} else {
+				colorToCheck = "b";
+			}
+			
+			if (!board[dstRow][dstCol].contains(colorToCheck) && !board[dstRow][dstCol].trim().equals("") && !board[dstRow][dstCol].trim().equals("##")) {
+				return false;
+			} else {
+				return true;
+			}
+			
+		} else {
+			return false;
+		}
+		
 	}
 
 }
